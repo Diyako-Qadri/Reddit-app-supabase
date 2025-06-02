9a51023e5c5e22c6de85559ea16a7d388fd1bd3d
Diyako-Qadri <qadri.diyako@gmail.com>
2025-06-02 14:06:33 +0200

    feat: added quey get posts

--- utils/supabase/queries.ts
+++ utils/supabase/queries.ts
@@ -1,8 +1,21 @@
 import 'server-only';
 
-import { type QueryData } from '@supabase/supabase-js';
+import { createClient } from "./client";
+import { QueryData } from "@supabase/supabase-js";
 
-import { createClient } from './server';
+
+
+export const getSinglePost = async (
+  supabase: ReturnType<typeof createClient>,
+  slug: string
+) => {
+  return supabase
+    .from("posts")
+    .select('id, title, content, user_id, users("email", "profile_image", "user_name"), slug, image')
+    .eq("slug", slug)
+    .single();
+};
+
 
 export const getHomePosts = async () => {
   const supabase = createClient();
