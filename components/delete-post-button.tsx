01b1f720696d45e6cf331c9a8b3f59b21e321bff
Diyako-Qadri <qadri.diyako@gmail.com>
2025-06-02 14:07:28 +0200

    feat: create delete btn for posts

---
+++ components/delete-post-button.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import { Loader, Trash2 } from 'lucide-react';
+import { deletePost } from '@/actions/delete-post';
+import  Button  from './button';
+
+export const DeletePostButton = ({ postId }: { postId: string }) => {
+  const { mutate, isPending } = useMutation({
+    mutationFn: () => deletePost(postId),
+    onError: error => toast.error(error.message),
+    onSuccess: () => toast.success('your post was deleted!'),
+    onMutate: () => toast.loading('deleting post...'),
+    onSettled: () => toast.dismiss(),
+  });
+
+  return (
+    <div onClick={() => mutate()}>
+      {isPending ? (
+        <Loader size={20} className="animate-spin" />
+      ) : (
+        <Trash2
+          className="transition-all cursor-pointer  hover:text-red-600"
+          size={20}
+        />
+      )}
+    </div>
+  );
+};
